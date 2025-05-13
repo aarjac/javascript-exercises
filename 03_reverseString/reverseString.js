d483f46cf6cf7d91235c7e10e1aab00425c29282
aarjac <56480900+aarjac@users.noreply.github.com>
2025-05-13 09:14:57 -0400

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    
+    let stringReversed='';
+    if(word.length>0) {
+        for(i=word.length; i>=0; i--) {
+            stringReversed+=word.charAt(i);
+        }
+    }
+    return stringReversed;
 };
 
 // Do not edit below this line
