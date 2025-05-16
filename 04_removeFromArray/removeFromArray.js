4a090c7891a015e9dd55c3d51e9aeb36601fc4ed
aarjac <56480900+aarjac@users.noreply.github.com>
2025-05-16 13:33:51 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(anArray, toRemove, ...restParam) {
+  for(i=0; i<anArray.length; i++) {
+    if(anArray[i]===toRemove) {
+        anArray.splice(i, 1);
+        i--;
+    }  
+  }
+  if(restParam.length>0) {
+    for(j=0; j<restParam.length; j++) {
+        let removeAnother=restParam[j];
+        for(k=0; k<anArray.length; k++) {
+            if(anArray[k]===removeAnother) {
+                anArray.splice(k, 1);
+            }
+        }
+    }
+  }
+
+  return anArray;
 };
 
 // Do not edit below this line
