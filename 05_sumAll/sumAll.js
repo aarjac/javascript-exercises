71ce0910823e6de27650d19a31f67dd1c2b2ec82
aarjac <56480900+aarjac@users.noreply.github.com>
2025-05-16 20:26:07 -0400

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum=0;
+    if(a>=0 && b>=0 
+        && Number.isInteger(a) && Number.isInteger(b)
+        && typeof(a)=='number' && typeof(b)=='number') {
+        switch(a<b) {
+            case true: 
+            for(toAdd=a; toAdd<=b; toAdd++) {
+                sum+=toAdd;
+            }
+            break;
+            case false:
+            for(toAdd=b; toAdd<=a; toAdd++) {
+                sum+=toAdd;
+            }    
+            break;
+        }  
+    } else {
+        sum='ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
